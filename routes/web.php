deca4171ce344985dd9b8f6ed321c846fa92217d
arapruy <72084746+arapruy@users.noreply.github.com>
2021-03-05 13:11:13 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,12 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
 
-<<<<<<< HEAD
 Route::inertia('/sorteos', 'sorteos/sorteo')->name('sorteos');
 route::get('/formulario', [InscripcionController::Class, 'index']);
 route::post('/formulario', [InscripcionController::Class, 'store']);
-=======
+
 //Route::post('/InscripcionController', function (\Illuminate\Http\Request $request){ $request->dd(); });
 
 
 route::get('/formulario', [InscripcionController::class, 'index']);
 route::post('/formulario', [InscripcionController::class, 'store']);
->>>>>>> main
